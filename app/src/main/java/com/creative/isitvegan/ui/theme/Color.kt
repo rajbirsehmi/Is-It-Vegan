d06bf0be0a0630f222d07909f7321c88f65aede2
Rajbir Sehmi <raj.sehmi1@yahoo.com>
2026-05-06 14:47:28 -0700

    Updated app's theme

--- app/src/main/java/com/creative/isitvegan/ui/theme/Color.kt
+++ app/src/main/java/com/creative/isitvegan/ui/theme/Color.kt
@@ -2,10 +2,55 @@ package com.creative.isitvegan.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Modern Vegan Palette - Light
+val ForestGreen = Color(0xFF2D5A27)
+val SageGreen = Color(0xFF8CAF88)
+val EarthyBrown = Color(0xFF4A3728)
+val LeafGreen = Color(0xFF4CAF50)
+val Cream = Color(0xFFF9FBF7)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Light Theme Colors
+val LightPrimary = ForestGreen
+val LightOnPrimary = Color.White
+val LightPrimaryContainer = Color(0xFFD7E8D5)
+val LightOnPrimaryContainer = Color(0xFF0C1F0A)
+val LightSecondary = EarthyBrown
+val LightOnSecondary = Color.White
+val LightSecondaryContainer = Color(0xFFF0EAE3)
+val LightOnSecondaryContainer = Color(0xFF251A11)
+val LightTertiary = Color(0xFF2E658E)
+val LightOnTertiary = Color.White
+val LightTertiaryContainer = Color(0xFFD1E4FF)
+val LightOnTertiaryContainer = Color(0xFF001D36)
+val LightError = Color(0xFFBA1A1A)
+val LightOnError = Color.White
+val LightBackground = Cream
+val LightOnBackground = Color(0xFF191C19)
+val LightSurface = Cream
+val LightOnSurface = Color(0xFF191C19)
+val LightSurfaceVariant = Color(0xFFDFE4D7)
+val LightOnSurfaceVariant = Color(0xFF43483E)
+val LightOutline = Color(0xFF73796D)
+
+// Dark Theme Colors
+val DarkPrimary = SageGreen
+val DarkOnPrimary = Color(0xFF0C390E)
+val DarkPrimaryContainer = Color(0xFF235123)
+val DarkOnPrimaryContainer = Color(0xFFBCE9B6)
+val DarkSecondary = Color(0xFFD3C2B1)
+val DarkOnSecondary = Color(0xFF3E2D1E)
+val DarkSecondaryContainer = Color(0xFF564333)
+val DarkOnSecondaryContainer = Color(0xFFF0EAE3)
+val DarkTertiary = Color(0xFFA0CCF0)
+val DarkOnTertiary = Color(0xFF003354)
+val DarkTertiaryContainer = Color(0xFF004B76)
+val DarkOnTertiaryContainer = Color(0xFFD1E4FF)
+val DarkError = Color(0xFFFFB4AB)
+val DarkOnError = Color(0xFF690005)
+val DarkBackground = Color(0xFF191C19)
+val DarkOnBackground = Color(0xFFE2E3DD)
+val DarkSurface = Color(0xFF191C19)
+val DarkOnSurface = Color(0xFFE2E3DD)
+val DarkSurfaceVariant = Color(0xFF43483E)
+val DarkOnSurfaceVariant = Color(0xFFC3C8BC)
+val DarkOutline = Color(0xFF8D9287)
