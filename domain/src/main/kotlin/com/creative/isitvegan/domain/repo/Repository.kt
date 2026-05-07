3a5790a3ae50238c0e806355d11b2ae5d47a9faf
Rajbir Sehmi <raj.sehmi1@yahoo.com>
2026-05-07 03:03:09 -0700

    Updated Some files

--- domain/src/main/kotlin/com/creative/isitvegan/domain/repo/Repository.kt
+++ domain/src/main/kotlin/com/creative/isitvegan/domain/repo/Repository.kt
@@ -1,12 +1,13 @@
 package com.creative.isitvegan.domain.repo
 
-import com.creative.isitvegan.data.local.entity.ProductEntity
-import com.creative.isitvegan.data.remote.dto.ProductResponse
+import com.creative.isitvegan.domain.model.ProductEntity
+import com.creative.isitvegan.domain.model.ProductResponse
+import kotlinx.coroutines.flow.Flow
 
 interface Repository {
     suspend fun getProduct(barcode: String): Result<ProductResponse>
     suspend fun saveProduct(product: ProductEntity): Long
     suspend fun getProductFromDb(barcode: String): ProductEntity?
-    fun getAllProducts(): kotlinx.coroutines.flow.Flow<List<ProductEntity>>
+    fun getAllProducts(): Flow<List<ProductEntity>>
     suspend fun deleteProduct(product: ProductEntity)
 }
